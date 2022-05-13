3d669d3cebd0c58f24515574b697c007d919b4c7
gabrielleite751 <hWhVRAEAPjUU2RC>
2022-05-13 20:11:15 -0300

    updated test cases

--- src/test/kotlin/school/cesar/criptocorretora/services/DollarServiceTest.kt
+++ src/test/kotlin/school/cesar/criptocorretora/services/DollarServiceTest.kt
@@ -1,7 +1,19 @@
 package school.cesar.criptocorretora.services
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 internal class DollarServiceTest{
 
+    private val dollarservicetest = DollarService()
+
+    @Test
+    fun `Deve receber um valor BigDecimal entre 4 e 5`() {
+        org.junit.jupiter.api.assertDoesNotThrow {
+            val valor = dollarservicetest.pegarValorDollarAtual()
+            assertTrue(valor >= BigDecimal(4))
+            assertTrue(valor <= BigDecimal(5))
+        }
+    }
 }
